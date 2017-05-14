31b9d1044be5d7442cc629cf14fde05be1456a91
Dieudonne <lx.simon@yahoo.com>
2017-05-14 15:36:56 +0800

    update openldap

--- openldap/scripts/ldap-entrypoint.sh
+++ openldap/scripts/ldap-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ulimit -n 8192
-current="$(cd "`dirname "$0"`"/..;pwd)"
 
 if [ ! -f "/var/lib/ldap/DB_CONFIG" ]; then 
     cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
